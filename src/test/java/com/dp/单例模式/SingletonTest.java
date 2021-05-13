b0d31fb853f507a333f906653fb95e5defe35a94
catface <zlyxzq@163.com>
2021-05-13 10:20:14 +0800

    [新增功能](master): first commit

---
+++ src/test/java/com/dp/单例模式/SingletonTest.java
@@ -0,0 +1,16 @@
+package com.dp.单例模式;
+
+import org.junit.Test;
+
+/**
+ * @author by catface
+ * @date 2021/5/13 10:17 上午
+ */
+public class SingletonTest {
+
+    @Test
+    public void test() {
+        Singleton1 singleton1 = new Singleton1();
+        System.out.println(singleton1);
+    }
+}
